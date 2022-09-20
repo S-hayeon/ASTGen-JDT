ae040f152fe89615a7d652cac5580d7a49082288
S-hayeon <hayeona92@gmail.com>
2022-09-20 17:56:33 +0900

    WIP JDTAST DTO

--- src/main/java/AST/BaseSerializer.java
+++ src/main/java/AST/BaseSerializer.java
@@ -12,24 +12,6 @@ import java.lang.reflect.Type;
 
 public class BaseSerializer implements JsonSerializer<ASTNode> {
 
-
-//    @Override
-//    public JsonElement serialize(final Base base, final Type type,
-//                                 final JsonSerializationContext context) {
-//
-//        final Operation operation = base.getOperation();
-//        final ASTLocation targetLocation = base.getTargetLocation();
-//        final JsonObject serializedBase = new JsonObject();
-//
-//        serializedBase.addProperty("name", operation.getName());
-//        serializedBase.add("fileName",
-//                context.serialize(targetLocation.getSourcePath()));
-//        serializedBase.addProperty("snippet", operation.getTargetSnippet());
-//        serializedBase.add("lineNumberRange", context.serialize(targetLocation.inferLineNumbers()));
-//
-//        return serializedBase;
-//    }
-
     @Override
     public JsonElement serialize(ASTNode src, Type typeOfSrc, JsonSerializationContext context) {
         return null;
